4f842f3a15b87799b3250c0ab47b2b0aa332bba8
robinlecorvaisier <robin.lecorvaisier@etu.umontpellier.fr>
2019-02-05 09:43:32 +0100

    exercice 1 "1 à 5 terminés" Signed-off-by: robinlecorvaisier <robin.lecorvaisier@etu.umontpellier.fr>

---
+++ src/main/java/fr/umontpellier/iut/exo1/Etudiant.java
@@ -0,0 +1,54 @@
+package fr.umontpellier.iut.exo1;
+
+import java.time.LocalDate;
+
+public class Etudiant {
+
+    private String Nom;
+    private String Prenom;
+    private LocalDate Dateofbirth;
+    private String Mail;
+    private String Address;
+
+
+   /* public Etudiant (String Nom, String Prenom, LocalDate Dateofbirth)
+    {
+
+        this.Nom = Nom.toUpperCase();
+        this.Prenom = Prenom.toLowerCase();
+        this.Dateofbirth = Dateofbirth;
+        generateDefaultAddressMail();
+    }
+    public Etudiant(String Nom, String Prenom, LocalDate Dateofbirth, String address)
+    {
+        this(Nom, Prenom, Dateofbirth);
+        Address = address;
+    }*/
+
+    public Etudiant(String nom, String prenom, LocalDate dateofbirth, String mail, String address) {
+        Nom = nom;
+        Prenom = prenom;
+        Dateofbirth = dateofbirth;
+        Mail = mail;
+        Address = address;
+    }
+
+    public String toString()
+    {
+        return Nom + " " + Prenom + "\n"+
+                "Date de naissance : "+Dateofbirth.toString() + "\n" +
+                "Adresse Mail : " + Mail + "\n" +
+                "Adresse : "+Address;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+   /* private void generateDefaultAddressMail()
+    {
+        Mail = Prenom.toLowerCase() + "." + Nom.toLowerCase() + "@etu.umontpellier.fr";
+    }*/
+
+
+}
--- src/main/java/fr/umontpellier/iut/exo1/GestionEtudiants.java
+++ src/main/java/fr/umontpellier/iut/exo1/GestionEtudiants.java
@@ -1,7 +1,17 @@
 package fr.umontpellier.iut.exo1;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class GestionEtudiants {
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented");
+        Etudiant toto = new Etudiant("dupoint","toto", LocalDate.of(2013, Month.MAY, 12),"toto.dupoint@etu.umontpellier", "rue pere noel");
+        System.out.println(toto);
+        Etudiant lolo = new Etudiant("dupoint", "toto", LocalDate.of(2013, Month.MAY, 12),"toto.dupoint@etu.umontpellier","rue pere noel");
+        System.out.println(lolo);
+
+        //System.out.println(lolo == toto); ne fonctionne pas, il compare l'adresse de l'object.
+
+
     }
 }
